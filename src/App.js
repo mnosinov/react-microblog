c0df3325b1982678f2423a0f5a2187f6c3d18910
mnosinov <mnosinov@gmail.com>
2025-08-11 13:38:07 +0500

    more mocking

--- src/App.js
+++ src/App.js
@@ -1,21 +1,41 @@
 export default function App() {
-  const post = {
-    id: 1,
-    text: 'Hello, world!',
-    timestamp: 'a minute ago',
-    author: {
-      username: 'susan',
+  const posts = [
+    {
+      id: 1,
+      text: 'Hello, world!',
+      timestamp: 'a minute ago',
+      author: {
+        username: 'susan',
+      },
     },
-  }
+    {
+      id: 2,
+      text: 'Second post',
+      timestamp: 'an hour ago',
+      author: {
+        username: 'john',
+      },
+    },
+  ];
+
 
   return (
     <>
       <h1>Microblog</h1>
-      <p>
-        <b>{post.author.username}</b> &mdash; {post.timestamp}
-        <br/>
-        {post.text}
-      </p>
+      {
+        posts.length === 0 ?
+          <p>There are no blog posts.</p>
+        :
+          posts.map(post => {
+            return (
+              <p key={post.id}>
+                <b>{post.author.username}</b> &mdash; {post.timestamp}
+                <br/>
+                {post.text}
+              </p>
+            );
+          })
+      }
     </>
   );
 }
