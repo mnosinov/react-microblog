cb77191069d7947806437a9f36315101a4de5ecb
mnosinov <mnosinov@gmail.com>
2025-08-12 07:38:45 +0500

    header, sidebar, body added

---
+++ src/components/Hearder.js
@@ -0,0 +1,13 @@
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+
+export default function Header() {
+  return (
+    <Navbar bg="light" sticky="top" className="Header">
+      <Container>
+        <Navbar.Brand>Mircoblog</Navbar.Brand>
+      </Container>
+    </Navbar>
+
+  );
+}
