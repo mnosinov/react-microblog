ff4f09d889549666dbb3c92a2eb9313a6309f0ae
mnosinov <mnosinov@gmail.com>
2025-08-14 17:26:52 +0500

    useState useEffect applied

---
+++ src/components/Post.js
@@ -0,0 +1,22 @@
+import { Image, Stack } from "react-bootstrap";
+import { Link } from "react-router-dom";
+
+
+export default function Post({ post }) {
+  return (
+    <Stack direction="horizontal" gap={3} className="Post">
+      <Image src={post.author.avatar_url + '&s=48'}
+        alt={post.author.username} roundedCircle />
+      <div>
+        <p>
+          <Link to={'/user/' + post.author.username}>
+            {post.author.username}
+          </Link>
+          &nbsp;&mdash;&nbsp;
+          {post.timestamp};
+        </p>
+        <p>{post.text}</p>
+      </div>
+    </Stack>
+  );
+}
