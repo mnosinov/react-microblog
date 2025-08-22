d227bd69f39455dac8a44c2076c882e39fe1dd18
mnosinov <mnosinov@gmail.com>
2025-08-22 13:33:49 +0500

    posts has been generalized

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -3,13 +3,27 @@ import Spinner from 'react-bootstrap/Spinner';
 import { useApi } from '../contexts/ApiProvider';
 import Post from './Post';
 
-export default function Posts() {
+export default function Posts({ content }) {
   const [posts, setPosts] = useState();
   const api = useApi();
 
+  let url;
+  switch (content) {
+    case 'feed':
+    case undefined:
+      url = '/feed';
+      break;
+    case 'explore':
+      url = '/posts'
+      break;
+    default:
+      url = `/users/${content}/posts`;
+      break;
+  }
+
   useEffect(() => {
     (async () => {
-      const response = await api.get('/feed');
+      const response = await api.get(url);
       if (response.ok) {
         setPosts(response.body.data);
       }
@@ -17,7 +31,7 @@ export default function Posts() {
         setPosts(null);
       }
     })();
-  }, [api]);
+  }, [api, url]);
 
   return (
     <>
